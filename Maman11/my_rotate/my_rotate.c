4513a7fe4f325370858c075b151819257c301aeb
Mark <markkapiki@gmail.com>
2022-11-18 14:27:57 +0200

    added doc to my_rotate.c, and done

--- Maman11/my_rotate/my_rotate.c
+++ Maman11/my_rotate/my_rotate.c
@@ -1,3 +1,11 @@
+/*
+// The my_rotate program:
+// The purpose of the program is to receive a number positive negative or equal to zero, and another
+ number of rotation the user wants to make as input from the user,
+// convert the number to binary and rotate it as much as the user wanted, convert it to back to decimal.
+// And as an output to print the representation of the number in hexadecimal, octal, and binary representations.
+ // meaning base 16,8,2 for the number that is the user input and one for the rotated one.
+*/
 #include "helper_functions.h"
 
 int main()
