e5320d5eec8da7664a1928e740ec11cb9791acb7
Mark <markkapiki@gmail.com>
2022-12-16 17:18:27 +0200

    finished before doc

--- Maman22/myset.c
+++ Maman22/myset.c
@@ -29,22 +29,28 @@ int check_input_numbers(int *user_input, int len){
     int real_number_length = len-1;
     for(;number_idx<real_number_length;number_idx++){
         if((user_input[number_idx] < 0 || user_input[number_idx] > 127) && (number_idx != real_number_length-1)){
-            printf("Numbers are not in the wanted range [0,127]\n");
-            return -1;
-        }else if((user_input[number_idx] == -1) && (number_idx != real_number_length-1)) {
-            printf("-1 is not in the right place which is the last place\n");
+            printf("Invalid set member - value out of range\n");
             return -1;
         }else if(user_input[real_number_length-1]!=-1){
-            printf("-1 is not closing the read option or you entered something different then integer\n");
+            printf("List of set members is not terminated correctly or you entered something different then integer\n");
             return -1;
         }
     }
     return 0;
 }
+int check_if_not_null_set(int *user_input, int len) {
+    int number_idx = 0;
+    int real_number_length = len-1;
+    for(;number_idx<real_number_length;number_idx++){
+        if(user_input[number_idx] != 0){
+            return 0;
+        }
+    }
+    return -1;
+}
 
 int check_set_name(char *user_input){
     int set_idx;
-    printf("got %s", user_input);
     for(set_idx=0; set_idx < NUMBER_OF_SETS; set_idx++){
         if(memcmp(user_input, set_names[set_idx], 4) == 0)
             return set_idx;
@@ -69,8 +75,11 @@ int main(){
     int operation_idx;
     int name_check;
     int comma_counter = 1;
+    int is_set_not_null = 0;
     char *user_input_pointer;
-    set* user_input_set;
+    set* user_first_input_set;
+    set* user_second_input_set;
+    set* user_third_input_set;
 
     if(intialize_all_sets() == 1){
         printf("exiting\n");
@@ -93,21 +102,21 @@ int main(){
     printf("prompt: \n");
     while (fgets(user_input, MAX_LENGTH, stdin) != NULL) {
         user_input_pointer = user_input;
-        printf("input: %s", user_input_pointer);
         while (isspace(*user_input_pointer)) {
             ++user_input_pointer;
         }
         operation_idx = check_command(user_input_pointer);
         if (operation_idx != -1) {
-            printf("operation idx: %d\n", operation_idx);
             if (operation_idx == 0) {
                 number_idx = 0;
                 user_input_pointer += strlen(valid_operations[operation_idx]);
-                printf("wanted: %s\n", user_input_pointer);
                 while (isspace(*user_input_pointer)) {
                     ++user_input_pointer;
                 }
-                if(*user_input_pointer == ','){
+                if(*user_input_pointer == '\0'){
+                    printf("Missing parameter\n");
+                }
+                else if(*user_input_pointer == ','){
                     printf("Illegal comma\n");
                 }
                 else{
@@ -118,7 +127,7 @@ int main(){
                             ++user_input_pointer;
                         }
                         if (*user_input_pointer != ',') {
-                            printf("You didnt put coma before reading the numbers\n");
+                            printf("Missing comma\n");
                         } else {
                             while (*user_input_pointer != '\0') {
                                 while (isspace((*user_input_pointer)) || *user_input_pointer == ',') {
@@ -130,35 +139,37 @@ int main(){
                                 user_input_numbers[number_idx] = strtol(user_input_pointer, &user_input_pointer, 10);
                                 number_idx ++;
                             }
-                            printf("%c\n\n",*(user_input_pointer-2) );
+                            is_set_not_null = check_if_not_null_set(user_input_numbers, number_idx);
                             if (check_input_numbers(user_input_numbers, number_idx) != 0) {
                                 printf("\n");
                             }
-                            else if((*(user_input_pointer-2) != '1')){
+                            else if((*(user_input_pointer-2) != '1') && is_set_not_null == 0){
                                 printf("Extraneous text after end of command\n");
                             }
-                            else if(comma_counter != number_idx){
+                            else if((comma_counter != number_idx) && (is_set_not_null == 0)){
                                 printf("Multiple consecutive commas\n");
                             }
                             else if (check_input_numbers(user_input_numbers, number_idx) == 0) {
-                                user_input_set = sets[name_check];
-                                memset(user_input_set, 0, 16);
-                                read_set(user_input_set, user_input_numbers, number_idx);
+                                user_first_input_set = sets[name_check];
+                                memset(user_first_input_set, 0, 16);
+                                read_set(user_first_input_set, user_input_numbers, number_idx);
                             }
                             comma_counter = 1;
                         }
                 }else {
-                        printf("You entered a wrong set name\n");
+                        printf("Undefined set name\n");
                 }
             }
             }
             else if (operation_idx == 1) {
             user_input_pointer += strlen(valid_operations[operation_idx]);
-            printf("wanted: %s\n", user_input_pointer);
             while (isspace(*user_input_pointer)) {
                 ++user_input_pointer;
             }
-            if(*user_input_pointer == ','){
+            if(*user_input_pointer == '\0'){
+                printf("Missing parameter\n");
+            }
+            else if(*user_input_pointer == ','){
                 printf("Illegal comma\n");
             }
             else{
@@ -166,36 +177,118 @@ int main(){
                 if (name_check != -1) {
                     printf("Printing the set %s: \n", set_names[name_check]);
                     user_input_pointer += 4;
-                    user_input_set = sets[name_check];
+                    user_first_input_set = sets[name_check];
                     if(*(user_input_pointer+1) != '\0'){
                         printf("Extraneous text after end of command\n");
                     }
                     else{
-                        print_set(user_input_set);
+                        print_set(user_first_input_set);
                     }
                 } else {
-                    printf("Entered a wrong set name\n");
+                    printf("Undefined set name\n");
                 }
             }
         }else if(operation_idx == 2 || operation_idx == 3 || operation_idx == 4 || operation_idx == 5){
+            user_input_pointer += strlen(valid_operations[operation_idx]);
+            while (isspace(*user_input_pointer)) {
+                ++user_input_pointer;
+            }
+            if(*user_input_pointer == '\0'){
+                printf("Missing parameter\n");
+            }
+            else if(*user_input_pointer == ','){
+                printf("Illegal comma\n");
+            }
+            else {
+                name_check = check_set_name(user_input_pointer);
+                user_input_pointer += 4;
+                if (name_check != -1) {
+                    user_first_input_set = sets[name_check];
+                    while (isspace(*user_input_pointer)) {
+                        ++user_input_pointer;
+                    }
+                    if (*user_input_pointer == '\0') {
+                        printf("Missing parameter \n");
+                    }else if(*user_input_pointer != ','){
+                        printf("Missing comma\n");
+                    }else{
+                        ++user_input_pointer;
+                        while(isspace(*user_input_pointer)){
+                            ++user_input_pointer;
+                        }
+                        if(*user_input_pointer == '\0'){
+                            printf("Missing parameter\n");
+                        }
+                        else if (*user_input_pointer == ',') {
+                            printf("Multiple comma \n");
+                        }
+                        else{
+                            name_check = check_set_name(user_input_pointer);
+                            user_input_pointer+=4;
+                            if(name_check!=-1){
+                                user_second_input_set = sets[name_check];
+                                while (isspace(*user_input_pointer)) {
+                                    ++user_input_pointer;
+                                }
+                                if(*user_input_pointer == '\0'){
+                                    printf("Missing parameter\n");
+                                }
+                                else if (*user_input_pointer != ',') {
+                                    printf("Missing comma \n");
+                                }else{
+                                    ++user_input_pointer;
+                                    while (isspace(*user_input_pointer)) {
+                                        ++user_input_pointer;
+                                    }
+                                    if(*user_input_pointer == '\0'){
+                                        printf("Missing parameter\n");
+                                    }
+                                    else if(*user_input_pointer==',') {
+                                        printf("Multiple commas\n");
+                                    }
+                                    else{
+                                        name_check = check_set_name(user_input_pointer);
+                                        user_input_pointer+=4;
+                                        if(name_check!=-1){
+                                            user_third_input_set = sets[name_check];
+                                            while (isspace(*user_input_pointer)) {
+                                                ++user_input_pointer;
+                                            }
+                                            if(*user_input_pointer != '\0'){
+                                                printf("Extraneous text after end command\n");
+                                            }else{
+                                                if(operation_idx == 2){
+                                                    printf("union_set\n");
+                                                    union_set(user_first_input_set, user_second_input_set, user_third_input_set);
+                                                }else if(operation_idx == 3){
+                                                    printf("intersect_set\n");
+                                                    intersect_set(user_first_input_set, user_second_input_set, user_third_input_set);
+                                                }
+                                                else if(operation_idx == 4){
+                                                    printf("sub_set");
+                                                    sub_set(user_first_input_set, user_second_input_set, user_third_input_set);
+                                                }
+                                                else{
+                                                    printf("symdiff_set\n");
+                                                    symdiff_set(user_first_input_set, user_second_input_set, user_third_input_set);
+                                                }
+                                            }
 
-            if(operation_idx == 2){
-                printf("union_set\n");
-                union_set(sets[0], sets[1], sets[2]);
-            }else if(operation_idx == 3){
-                printf("intersect_set\n");
-                intersect_set(sets[0], sets[1], sets[2]);
-            }
-            else if(operation_idx == 4){
-                printf("sub_set");
-                sub_set(sets[0], sets[1], sets[2]);
-            }
-            else{
-                printf("symdiff_set\n");
-                symdiff_set(sets[0], sets[1], sets[2]);
-            }
-        }
-        else if(operation_idx == 6){
+                                    }else{
+                                            printf("Undefined set name\n");
+                                        }
+                                    }
+                                }
+                        }else{
+                                printf("Undefined set name\n");
+                            }
+                        }
+                    }
+            }else{
+                    printf("Undefined set name\n");
+                }
+                }
+            }else if(operation_idx == 6){
             printf("stopping \n");
             break;
         }
